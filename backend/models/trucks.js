3eb2112e364bb5f9f9edb37d271fb7cd8c6387f0
James Wang <114769089+jameswang08@users.noreply.github.com>
2023-11-11 16:15:02 -0800

    Append process.env to .env variables

--- backend/models/trucks.js
+++ backend/models/trucks.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 // Set up mongoose connection
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const mongoDB = secretURL;
+const mongoDB = process.env.secretURL;
 
 main().catch((err) => console.log(err)).finally(() => {console.log("Hello World!")});
 async function main() {
