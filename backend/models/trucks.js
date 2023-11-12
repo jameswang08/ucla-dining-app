bab3c07fbc3c8f62b97a626f6cebe6836659eeb0
James Wang <114769089+jameswang08@users.noreply.github.com>
2023-11-11 16:11:45 -0800

    Store MongoDB connection string in .env file for security

--- backend/models/trucks.js
+++ backend/models/trucks.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
+
 // Set up mongoose connection
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const mongoDB = "mongodb+srv://ucladiningapp:ucladiningapp@ucla-dining-app.vzxgxgy.mongodb.net/?retryWrites=true&w=majority";
+const mongoDB = secretURL;
 
 main().catch((err) => console.log(err)).finally(() => {console.log("Hello World!")});
 async function main() {
