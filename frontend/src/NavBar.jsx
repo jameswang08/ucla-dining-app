5961d2acb4d6db6152755771c8238000445adaad
Amanda Xu <amandaminioncupcakes@gmail.com>
2023-11-11 17:27:05 -0800

    Adding home page top bar, and navbar template

---
+++ frontend/src/NavBar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function NavBar() {
+    return (
+    <>
+        <div className="navbar bg-base-100">
+            <div className="flex-1">
+                <a className="btn glass bg-base-100 normal-case text-xl">BruinEats</a>
+            </div>
+
+            <div className="dropdown dropdown-hover">
+                    <label tabIndex={0} className="btn glass"> At a Glance </label>
+                    <ul tabIndex={0} className="text-black dropdown-content z-[1] menu shadow p-2 bg-base-100 rounded-box w-48">
+                        <li><a>Truck 1</a></li>
+                        <li><a>Truck 2</a></li>
+                    </ul>
+            </div>
+            
+            <div>
+                <ul className="btn btn-active btn-link">Login</ul>
+            </div>
+        </div>
+    </>
+    );
+};
