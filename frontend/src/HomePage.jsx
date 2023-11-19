0d1679a3f9706216de63858cfc1c106ba8e89377
Amanda Xu <amandaminioncupcakes@gmail.com>
2023-11-18 16:03:36 -0800

    home page without image yet implemented

--- frontend/src/HomePage.jsx
+++ frontend/src/HomePage.jsx
@@ -1,15 +1,37 @@
 import React from "react";
-import NavBar from "./NavBar.jsx";
-import Box from "@mui/material/Box";
-import AppBar from "@mui/material/AppBar";
+import NavBar from "./NavBar.jsx"
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import AppBar from '@mui/material/AppBar';
+import '../dist/output.css'
 
 export default function HomePage() {
-  return (
-    <>
-      <AppBar>
-        <NavBar />{" "}
-      </AppBar>
-      <Box sx={{ bgcolor: "white", height: "100vh" }}></Box>
-    </>
-  );
+    return (
+        <div>
+            <div>
+                <NavBar />
+            </div>
+
+            <div class="text-white text-4xl ps-32 my-60 leading-[4rem]">
+                <pre> Your one-stop shop for</pre>
+                <pre> UCLA dining reviews.</pre>
+            </div>
+
+            <div class="text-black bg-light-yellow ps-40 py-7 my-40 leading-[2rem]">
+                <pre class='text-2xl font-bold leading-[2rem]'>Our goals{'\n\n'}</pre>
+                
+                <pre class='leading-[2rem]'>We believe in simplifying food for Bruins. {'\n'} </pre>
+                <pre class='leading-[1rem]'>How we do this? {'\n\n'} </pre>
+                <pre class='leading-[2rem]'>By helping Bruins help Bruins navigate on-campus dining.{'\n'}
+                    We offer a digital space for students to review UCLA’s dining {'\n'}
+                    halls and takeout restaurants and benefit current and future {'\n'}
+                    students alike.</pre>
+            </div>
+
+        
+        {/* <Container maxWidth={false} disableGutters>
+                
+            </Container> */}
+        </div>
+    );
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
       'dark-yellow': '#fcd34d',
       'black': '#020617',
       'white': '#fafafa',
+<<<<<<< HEAD
       'light-yellow': '#fde68a',
       'medium-grey': '#3f3f46',
       'light-grey': '#71717a',
@@ -16,5 +17,17 @@ module.exports = {
   daisyui: {
     base:false
   },
+=======
+      'light-yellow': '#fde68a'
+    }
+  },
+  plugins: [
+    require('@tailwindcss/typography'),require("daisyui")],
+
+  daisyui: {
+    base:false
+  }
+  
+>>>>>>> a632d55 (home page without image yet implemented)
 }
 
