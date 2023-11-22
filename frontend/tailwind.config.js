6554a1b1f0956d18c2c46991352317a9901ed002
Amanda Xu <amandaminioncupcakes@gmail.com>
2023-11-22 01:25:01 -0800

    Added Folders Structure and finished up static login page

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,7 +7,6 @@ module.exports = {
       'dark-yellow': '#fcd34d',
       'black': '#020617',
       'white': '#fafafa',
-<<<<<<< HEAD
       'light-yellow': '#fde68a',
       'medium-grey': '#3f3f46',
       'light-grey': '#71717a',
@@ -17,17 +16,5 @@ module.exports = {
   daisyui: {
     base:false
   },
-=======
-      'light-yellow': '#fde68a'
-    }
-  },
-  plugins: [
-    require('@tailwindcss/typography'),require("daisyui")],
-
-  daisyui: {
-    base:false
-  }
-  
->>>>>>> a632d55 (home page without image yet implemented)
 }
 
