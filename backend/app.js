5eefa39cb29426f07cdb18e479cf17e7153cf193
James Wang <114769089+jameswang08@users.noreply.github.com>
2023-11-25 14:13:25 -0800

    Clean up backend to remove unecessary files, since a view engine is no longer being used

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,13 @@
-const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const mongoose = require("mongoose");
 const UserModel = require('./models/users');
 const TrucksModel = require('./models/trucks');
 require('dotenv').config();
 
-const indexRouter = require('./routes/index');
-
 const app = express();
 
 // Set up mongoose connection
-const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 const mongoDB = process.env.secretURL;
 
@@ -36,32 +31,15 @@ async function main() {
   //   });
 }
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.use(express.json())
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.get("/", async (req, res) => {
+  res.send("Hello World")
+})
 
-app.use('/', indexRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
 
 module.exports = app;
