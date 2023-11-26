a268b486fa398d9b0daf6a2c56e4a34d4eaf7931
Amanda Xu <amandaminioncupcakes@gmail.com>
2023-11-25 17:27:56 -0800

    forms for creatingn account and login

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -8,25 +8,44 @@ import { eyeOff } from "react-icons-kit/feather/eyeOff";
 import { eye } from "react-icons-kit/feather/eye";
 
 export default function LoginPage() {
-  const [password, setPassword] = useState("");
+  const [inputs, setInputs] = useState({});
   const [showPassword, setShowPassword] = useState(false);
 
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(inputs);
+  };
+
   return (
-    <div>
+    <form onSubmit={handleSubmit}>
       <div>
         <NavBar />
       </div>
 
       <div class="text-white ml-28 mt-32">
-        <pre class="text-3xl leading-[3rem]">Login{"\n"}</pre>
+        <pre className="text-3xl leading-[3rem]">Login{"\n"}</pre>
 
-        <pre class="leading-[2.75rem]">{"\n"}</pre>
+        <pre className="leading-[2.75rem]">{"\n"}</pre>
 
-        <pre class="leading-[2.5rem]">{"\n"}</pre>
+        <pre className="leading-[2.5rem]">{"\n"}</pre>
 
         {/* username */}
         <label id="username_label">
-          <input type="text" class="bg-gray" placeholder=" " id="username" />
+          <input
+            type="text"
+            className="bg-gray"
+            placeholder=" "
+            id="username"
+            name="username"
+            value={inputs.username || ""}
+            onChange={handleChange}
+          />
           <span id="username_span"> Username </span>
         </label>
 
@@ -37,9 +56,10 @@ export default function LoginPage() {
           <label id="user_password_label">
             <input
               type={showPassword ? "text" : "password"}
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              class="user_password bg-gray"
+              value={inputs.password || ""}
+              onChange={handleChange}
+              className="bg-gray"
+              name="password"
               placeholder=" "
               id="user_password"
             />
@@ -47,7 +67,7 @@ export default function LoginPage() {
             <span id="user_password_span"> Password </span>
           </label>
         </div>
-        <ul class="btn btn-active btn-link text-blue text-sm ml-[-1rem] mt-[0.5rem] mb-[3rem]">
+        <ul className="btn btn-active btn-link text-blue text-sm ml-[-1rem] mt-[0.5rem] mb-[3rem]">
           Create an Account→
         </ul>
       </div>
@@ -58,16 +78,20 @@ export default function LoginPage() {
           type="checkbox"
           value={showPassword}
           onClick={() => setShowPassword((prev) => !prev)}
-          class="absolute text-white ml-[24rem] mt-[-8.8rem]"
+          className="absolute text-white ml-[24rem] mt-[-8.8rem]"
         >
           {showPassword ? <Icon icon={eye} /> : <Icon icon={eyeOff} />}
         </button>
       </div>
 
-      <ul class="btn btn-active btn-link text-light-yellow text-xl ml-[42rem]  mt-[3.5rem] mb-[3rem]">
+      <ul
+        type="submit"
+        onClick={handleSubmit}
+        className="btn btn-active btn-link text-light-yellow text-xl ml-[42rem]  mt-[3.5rem] mb-[3rem]"
+      >
         Login→
       </ul>
-    </div>
+    </form>
   );
 }
 
